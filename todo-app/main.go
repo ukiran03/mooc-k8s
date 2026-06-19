16a8c2ccc05aa5791ec5263c17300296444404c4
ukiran03 <ushakiranreddi@gmail.com>
2026-06-19 09:27:34 +0530

    completed 1.5. The project, step 3 exercise

--- todo-app/main.go
+++ todo-app/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"embed"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
+
+//go:embed ui/*.tmpl
+var templateFS embed.FS
+
+type PageData struct {
+	Title   string
+	Message string
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -15,10 +25,22 @@ func main() {
 	addr := ":" + port
 	http.HandleFunc("/", helloHandler)
 
-	log.Printf("Server started in port %s", port)
+	log.Printf("Server started on port %s", port)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Todo App: Hello world!"))
+	data := PageData{
+		Title:   "Hello from Todo-App",
+		Message: "This is from Exercise: 1.5",
+	}
+
+	tmpl, err := template.ParseFS(templateFS, "ui/index.tmpl")
+	if err != nil {
+		log.Printf("Template error: %v", err)
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, data)
 }
